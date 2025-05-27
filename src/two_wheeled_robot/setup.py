136068235a98c386ff3fd36aca420e9013448255
Elifina <de.liomia@yandex.ru>
2025-05-27 11:19:47 +0300

    Добавлен teleop_twist_keyboard в Dockerfile.base

---
+++ src/two_wheeled_robot/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+package_name = 'two_wheeled_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'teleop_node = two_wheeled_robot.teleop_node:main',
+        ],
+    },
+)
