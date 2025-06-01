6de5eb5a3df8ac79e293dcbdb4827eda9b5910b8
nikita1323 <mazhuga.nikita@icloud.com>
2025-06-01 20:36:51 +0300

    added autonomous driving via random_goal_generator using DWA planner

--- docker/run_docker_lab2.sh
+++ docker/run_docker_lab2.sh
@@ -7,8 +7,10 @@ docker run -it --rm \
     --env DISPLAY=${DISPLAY} \
     --env LIBGL_ALWAYS_SOFTWARE=1 \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
+    --volume $(pwd)/../src:/workspace/lab_2025/src \
     --device /dev/dri:/dev/dri \
     --runtime=nvidia \
     --gpus all \
     --rm \
+    --name lab2_docker \
     lab_2
